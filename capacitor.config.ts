1a58d26f3c6cc058c44ae6b60910095e43b5cd80
Vina del Mar <vina@duoc.acad>
2025-08-28 15:33:34 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pololitosApp',
+  webDir: 'www'
+};
+
+export default config;
