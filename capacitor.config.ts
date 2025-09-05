fb8adcdb7d896d29ec118eaa54b477ae2b058988
Vina del Mar <vina@duoc.acad>
2025-09-05 10:09:17 -0400

    diseño

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,13 +4,21 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'pololitos',
   webDir: 'www',
-  // --- INICIO DE LA SECCIÓN A AÑADIR ---
   plugins: {
+    // --- INICIO DE LA SECCIÓN A AÑADIR/MODIFICAR ---
+    SplashScreen: {
+      launchShowDuration: 3000, // Duración en milisegundos (3 segundos)
+      launchAutoHide: true, // Se oculta automáticamente
+      backgroundColor: '#0B1426', // Color de fondo que debe coincidir con tu splash
+      androidScaleType: 'CENTER_CROP', // Cómo se ajusta la imagen en Android
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+    // --- FIN DE LA SECCIÓN ---
     Keyboard: {
-      resize: 'none' // La opción 'none' evita que la vista se redimensione
+      resize: 'none'
     }
   }
-  // --- FIN DE LA SECCIÓN A AÑADIR ---
 };
 
 export default config;
