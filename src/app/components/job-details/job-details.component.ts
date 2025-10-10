ea0fbbda4ac8784200790f458b1a719140c49e70
Vina del Mar <vina@duoc.acad>
2025-10-10 11:58:43 -0300

    lol 2

--- src/app/components/job-details/job-details.component.ts
+++ src/app/components/job-details/job-details.component.ts
@@ -3,13 +3,20 @@ import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-job-details',
-  templateUrl: './job-details.component.html',
+  templateUrl: './job-details.component.html', // Corregido para que coincida con el nombre de archivo
   styleUrls: ['./job-details.component.scss'],
   standalone: false,
 })
-export class JobDetailsComponent {
+export class JobDetailsComponent { // El nombre de la clase ahora es JobDetailsComponent
   @Input() job: any;
-  constructor(private modalController: ModalController) { }
-  dismissModal() { this.modalController.dismiss(); }
-  contact() { this.modalController.dismiss(); }
+
+  constructor(private modalCtrl: ModalController) { }
+
+  dismissModal() {
+    this.modalCtrl.dismiss();
+  }
+
+  contact() {
+    console.log('Contactando por el trabajo:', this.job);
+  }
 }
--- src/app/components/job-details/job-details.module.ts
+++ src/app/components/job-details/job-details.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { JobDetailsComponent } from './job-details.component';
-import { LucideAngularModule, X, MapPin, Send } from 'lucide-angular';
+import { JobDetailsComponent } from './job-details.component'; // Importa el componente
+import { LucideAngularModule, X, MapPin, Calendar, Send } from 'lucide-angular';
 
 @NgModule({
-  declarations: [JobDetailsComponent],
-  imports: [
-    CommonModule,
+  imports: [ 
+    CommonModule, 
+    FormsModule, 
     IonicModule,
-    LucideAngularModule.pick({ X, MapPin, Send })
+    LucideAngularModule.pick({ X, MapPin, Calendar, Send })
   ],
-  exports: [JobDetailsComponent]
+  declarations: [JobDetailsComponent], // Lo declara
+  exports: [JobDetailsComponent]      // Lo exporta para que otros módulos puedan usarlo
 })
 export class JobDetailsComponentModule {}
